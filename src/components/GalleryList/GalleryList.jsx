940b7f3edf67a0f9fc5952d35152949f76455452
Stephen Vertucci <steve.vertucci@gmail.com>
2023-04-04 19:08:14 -0400

    created list and item components, started work on axios.get request

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'; 
+import {usestate, useEffect} from 'react'; 
+
+function GalleryList () {
+
+    const galleryList = () => {
+        axios.get('').then((response) => {
+            
+        })
+    }
+    return(
+        <></>
+    )
+}
