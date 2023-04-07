f2ca2d34d5d5fc5910d0b1219cc74a933220af4a
Stephen Vertucci <steve.vertucci@gmail.com>
2023-04-07 13:07:47 -0400

    forgot to add pictures to gallery.data, now appearing in DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/picture_two.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/picture_three.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/picture_four.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 5, path: 'images/picture_five.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
 ];
 
 module.exports = galleryItems;
