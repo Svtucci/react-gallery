170255030c6a72fe753f34aae60d847a56f93de5
Stephen Vertucci <steve.vertucci@gmail.com>
2023-04-07 15:05:17 -0400

    added picture descriptions

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 7 },
-    { id: 2, path: 'images/picture_two.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 5 },
-    { id: 3, path: 'images/picture_three.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 3 },
-    { id: 4, path: 'images/picture_four.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 2 },
-    { id: 5, path: 'images/picture_five.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 1 },
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park', likes: 7 },
+    { id: 2, path: 'images/picture_two.jpg', description: 'NationalPark Bayerischer Wald', likes: 5 },
+    { id: 3, path: 'images/picture_three.jpg', description: 'Summit of Alpspitze', likes: 3 },
+    { id: 4, path: 'images/picture_four.jpeg', description: 'On the way up the Hollentalanger', likes: 2 },
+    { id: 5, path: 'images/picture_five.jpg', description: 'Beede Falls, NY', likes: 1 },
 ];
 
 module.exports = galleryItems;
