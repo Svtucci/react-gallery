c2c0a6a1773c0ab9c8d61f6973ef043708ae6bc8
Stephen Vertucci <steve.vertucci@gmail.com>
2023-04-09 19:58:08 -0400

    got cards to work after having chatgpt explain how to implement, messed around with the sizing and layout

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,13 @@
 import axios from 'axios'; 
 import {useState, useEffect} from 'react'; 
 import Button from '@mui/material/Button';
+//big mui cards imports 
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 
 
 function GalleryItem ({item}) {
@@ -19,35 +26,96 @@ function GalleryItem ({item}) {
 const getDescription = () => {
     if (toggleDescription === true) {
         return (
-            <p onClick={() => setDescription(!toggleDescription)}>{'\"'+item.description+'\"'}</p>
+            <Typography onClick={() => setDescription(!toggleDescription)} variant="body2" color="text.secondary">
+                {'\"'+item.description+'\"'}
+            </Typography>
         )
     } else {
         return (
-            <img onClick={() => setDescription(!toggleDescription)} src={item.path} />
+            <CardMedia
+                component="img"
+                height="140"
+                image={item.path}
+                onClick={() => setDescription(!toggleDescription)}
+            />
         )
     }
 }
 // Used a concatonating string to add "" around descriptions ^^^
 
     return(
-        <>
-        <p>
-        {
-            getDescription()
-        }
-        </p>
-        <p>{likeCount} likes</p>
-        <p>
-        <Button variant = "outlined" onClick= {addLike}>Like</Button>
-        <br />
-        <br />
-        <Button variant= "outlined" onClick={() => setDescription(!toggleDescription)}>
-            {toggleDescription ? 'Image' : 'Description'}
-        </Button>
-        </p>
-        </>
+        <Card sx={{height: 400, width: 300}}>
+        <CardActionArea>
+            {getDescription()}
+        </CardActionArea>
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {item.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {likeCount} likes
+            </Typography>
+            <Button variant="outlined" onClick={addLike}>Like</Button>
+            <Button variant="outlined" onClick={() => setDescription(!toggleDescription)}>
+                {toggleDescription ? 'Image' : 'Description'}
+            </Button>
+        </CardContent>
+    </Card>
     )
 }
 
 
 export default GalleryItem
+
+
+// ORIGINAL CODE BEFORE MUI
+// import axios from 'axios'; 
+// import {useState, useEffect} from 'react'; 
+// import Button from '@mui/material/Button';
+
+
+// function GalleryItem ({item}) {
+//     const [likeCount, setLikeCount] = useState(item.likes)
+//     const [toggleDescription, setDescription] = useState(false); 
+
+//     const addLike = () => {
+//         axios.put(`/gallery/like/${item.id}`).then((response) => {
+//             setLikeCount(likeCount + 1);
+//     }).catch((error) => {
+//         console.log(`Error in POST ${error}`);
+//         alert('Something went wrong in POSt');
+//     });
+// };
+
+// const getDescription = () => {
+//     if (toggleDescription === true) {
+//         return (
+//             <p onClick={() => setDescription(!toggleDescription)}>{'\"'+item.description+'\"'}</p>
+//         )
+//     } else {
+//         return (
+//             <img onClick={() => setDescription(!toggleDescription)} src={item.path} />
+//         )
+//     }
+// }
+// // Used a concatonating string to add "" around descriptions ^^^
+
+//     return(
+//         <>
+//         <p>
+//         {
+//             getDescription()
+//         }
+//         </p>
+//         <p>{likeCount} likes</p>
+//         <p>
+//         <Button variant = "outlined" onClick= {addLike}>Like</Button>
+//         <br />
+//         <br />
+//         <Button variant= "outlined" onClick={() => setDescription(!toggleDescription)}>
+//             {toggleDescription ? 'Image' : 'Description'}
+//         </Button>
+//         </p>
+//         </>
+//     )
+// }
